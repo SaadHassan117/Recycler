686377f6b3ab65077092484a91eafeb830b6b7fc
SaadHassan117 <saadh9290@gmail.com>
2021-12-15 11:35:39 -0800

    recycle view

---
+++ app/src/main/java/com/example/recycler/Recy.java
@@ -0,0 +1,29 @@
+package com.example.recycler;
+
+public class Recy {
+    public Recy(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    private int a;
+    private String b;
+
+
+}
